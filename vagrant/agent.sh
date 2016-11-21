06541de05967ef6a66a1b4c1e6df7b6f020e0b88
Thilo Ettelt <thilo.ettelt@neuland-bfi.de>
2016-11-21 11:58:15 +0100

    Added nohup background hook

--- vagrant/agent.sh
+++ vagrant/agent.sh
@@ -13,4 +13,4 @@ sudo systemctl enable docker.service
 sudo systemctl start docker
 sudo docker run --rm hello-world
 sudo usermod -aG docker vagrant
-sudo mesos agent --master=10.111.111.10:5050 --work_dir=/var/lib/mesos-agent/ --log_dir=/var/log/mesos/ --containerizers=mesos,docker
+sudo mesos agent --master=10.111.111.10:5050 --work_dir=/var/lib/mesos-agent/ --log_dir=/var/log/mesos/ --containerizers=mesos,docker > mesos.log 2>&1 &
