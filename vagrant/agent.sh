9d179c730d95e027682bda67100ccaed86721da2
Thilo Ettelt <thilo.ettelt@neuland-bfi.de>
2016-11-21 11:42:34 +0100

    Vagrant box is now centos/7

---
+++ vagrant/agent.sh
@@ -0,0 +1,16 @@
+sudo rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+sudo yum -y install mesos
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+sudo yum install docker-engine
+sudo systemctl enable docker.service
+sudo systemctl start docker
+sudo docker run --rm hello-world
+sudo usermod -aG docker vagrant
+sudo mesos agent --master=10.111.111.10:5050 --work_dir=/var/lib/mesos-agent/ --log_dir=/var/log/mesos/ --containerizers=mesos,docker
--- vagrant/master.sh
+++ vagrant/master.sh
@@ -1,13 +1,10 @@
-# Setup
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-CODENAME=$(lsb_release -cs)
-
-# Add the repository
-echo "deb http://repos.mesosphere.com/ubuntu xenial64 main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
-  sudo apt-get -y update
-
-# Install packages
-sudo apt-get -y install mesos marathon zookeeper zookeeperd
-
-sudo mesos master --work_dir=/var/lib/mesos/ --log_dir=/var/log/mesos/ --zk=zk://localhost:2181/mesos --quorum=1
+sudo rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+sudo yum -y install mesos marathon wget
+sudo systemctl stop firewalld
+mkdir -p /var/zookeeper
+wget http://www-eu.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz
+tar -xvf zookeeper-3.4.9.tar.gz
+cd zookeeper-3.4.9
+sudo mv conf/zoo_sample.cfg conf/zoo.cfg
+sudo bin/zkServer.sh start
+sudo nohup mesos master --work_dir=/var/lib/mesos/ --log_dir=/var/log/mesos/ --zk=zk://localhost:2181/mesos --quorum=1 --ip=10.111.111.10 > mesos.log 2>&1 &
